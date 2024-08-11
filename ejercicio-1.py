b5dc8b0a721f184106d0d0327ceba882d02206c5
ramon <ramonguerraospino@hotmail.es>
2024-08-11 17:16:53 -0500

    at

--- ejercicio-1.py
+++ ejercicio-1.py
@@ -11,5 +11,16 @@ resultado =busqueda(lista,elemento)
 if resultado != -5:
     print(f"elemento {elemento} se encuentra en la posicion {resultado}")
 else:
-    print(f"el elemento{elemento} no se encuentra en la lista")    
+    print(f"el elemento{elemento} no se encuentra en la lista")  
     
+lista=lista[2]
+print(lista)
+
+list=[1,2,3,4,5,6]
+ele=4
+def bus(list,ele):
+    for i in range(len(list)):
+        if list[i]==ele:
+            return i
+res=bus(list,ele)    
+print(res)
