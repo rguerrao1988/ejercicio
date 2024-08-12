708d1fdee4b15f789c54ba638e5378b57617ea41
ramon <ramonguerraospino@hotmail.es>
2024-08-12 18:51:32 -0500

    parcticando codigo

--- def_y_for.py
+++ def_y_for.py
@@ -1,4 +1,4 @@
-
+# ejemplo
 
 num_b=int(input("numero a consultar? "))
 # ind  0 1 2 3 4 5 6 7 8 9 10
@@ -40,3 +40,5 @@ def resta(num_1=3,nume_2=2):
     return num_1 - nume_2
 total=resta()
 print(total)
+
+#ejemplo
